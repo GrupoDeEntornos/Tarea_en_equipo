6bbb942edbe47c2945184f5b5a0331ae6b062d66
Laura <lauraagudo1991@gmail.com>
2020-02-22 17:26:37 +0100

    aleatorio del 1 al 20

--- EjercicioUF3_03.java
+++ EjercicioUF3_03.java
@@ -1,15 +1,16 @@
-package ejerciciouf3_03;
+﻿package ejerciciouf3_03;
 
 public class EjercicioUF3_03 {
 
     public static void main(String[] args) {
-        int numero=(int)(Math.random()*10+1); // asigna valor al azar entre 1 y 10
+        int numero=(int)(Math.random()*20+1); // asigna valor al azar entre 1 y 20
         if(numero>5){
             System.out.println("Hoy va a ser un gran día");
         } 
         else{
             System.out.println("Hoy va a ser el mejor día del mundo");
         }
+	System.out.println("Fin del programa");
     }
     
 }
